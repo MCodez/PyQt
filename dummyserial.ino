22f834b2ccb8379f323964b6225d748ce517e9aa
MCodez <lalitarora1997@gmail.com>
2017-05-29 21:36:16 +0530

    MCodez
    Dummy Serial Code

---
+++ dummyserial.ino
@@ -0,0 +1,33 @@
+  /* 
+   *  Demo Code for sending 5 sensors readings.. 
+  */
+  float a=10;
+  float b=12;
+  float c=15;
+  float d=16;
+  float e=20;
+void setup() {
+Serial.begin(115200);
+}
+
+void loop() {
+a=a+1;
+b=b+1;
+c=c+1;
+d=d+1;
+e=e+1;
+
+Serial.print(a);
+Serial.print("  ");
+Serial.print(b);
+Serial.print("  ");
+Serial.print(c);
+Serial.print("  ");
+Serial.print(d);
+Serial.print("  ");
+Serial.print(e);
+Serial.print("  ");
+Serial.println();
+delay(500);
+
+}
